383eeb213e08b6bd46cc73939a84fd073aed0eb5
AndrewDem13 <andrewdem13@gmail.com>
2017-01-12 12:21:15 +0200

    Getting started

--- TrappingWater-Algorithm/TrappingWater.java
+++ TrappingWater-Algorithm/TrappingWater.java
@@ -1,7 +1,15 @@
+/**
+Problem from: https://leetcode.com/problems/trapping-rain-water/
+Given n non-negative integers representing an elevation map where the width of each bar is 1,
+compute how much water it is able to trap after raining.
+For example,
+Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+ */
 
 public class TrappingWater {
-    public static void main(String[] args) {
-
+    public static int compute(int[] array) {
+        int result = -1;
+        //TODO some magic with recursion
+        return result;
     }
-
 }
