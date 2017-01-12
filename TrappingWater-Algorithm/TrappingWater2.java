c66ad20d3e3685e18144542b52dd98573a66d666
AndrewDem13 <andrewdem13@gmail.com>
2017-01-12 19:06:38 +0200

    More compact and effective solution

---
+++ TrappingWater-Algorithm/TrappingWater2.java
@@ -0,0 +1,41 @@
+/**
+ Problem from: https://leetcode.com/problems/trapping-rain-water/
+ Given n non-negative integers representing an elevation map where the width of each bar is 1,
+ compute how much water it is able to trap after raining.
+ For example,
+ Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+ */
+
+// My second try of solution. Works well, requires less space and code looks more compact
+
+public class TrappingWater2 {
+    public static int compute(int[] array) {
+        if (array.length < 3)
+            return 0;
+
+        int result = 0;
+
+        // searching for left and right edge within which water can be trapped
+        int leftEdgeIndex = 0, rightEdgeIndex = array.length-1;
+        while (leftEdgeIndex < rightEdgeIndex && array[leftEdgeIndex] <= array[leftEdgeIndex +1])
+            leftEdgeIndex++;
+        while (rightEdgeIndex > leftEdgeIndex && array[rightEdgeIndex] <= array[rightEdgeIndex -1])
+            rightEdgeIndex--;
+
+        // moving from edges to center counting water
+        int leftNumber, rightNumber;
+        while (leftEdgeIndex < rightEdgeIndex) {
+            leftNumber = array[leftEdgeIndex];
+            rightNumber = array[rightEdgeIndex];
+            if (rightNumber >= leftNumber) { // current edges are the same, or left is lower (max height of water there)
+                while (leftEdgeIndex < rightEdgeIndex && leftNumber >= array[++leftEdgeIndex])
+                    result += leftNumber - array[leftEdgeIndex];
+            }
+            else { // current right edge is lover
+                while (leftEdgeIndex < rightEdgeIndex && rightNumber >= array[--rightEdgeIndex])
+                    result += rightNumber - array[rightEdgeIndex];
+            }
+        }
+        return result;
+    }
+}
--- TrappingWater-Algorithm/TrappingWaterTest.java
+++ TrappingWater-Algorithm/TrappingWaterTest.java
@@ -5,20 +5,20 @@ import static org.junit.Assert.*;
 public class TrappingWaterTest {
     @Test
     public void basic() {
-        assertEquals(6, TrappingWater.compute(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
-        assertEquals(15, TrappingWater.compute(new int[]{3,1,4,0,2,0,3,2,4}));
+        assertEquals(6, TrappingWater2.compute(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+        assertEquals(15, TrappingWater2.compute(new int[]{3,1,4,0,2,0,3,2,4}));
     }
 
     @Test
     public void shortArrays() {
-        assertEquals(0, TrappingWater.compute(new int[]{}));
-        assertEquals(0, TrappingWater.compute(new int[]{0}));
-        assertEquals(0, TrappingWater.compute(new int[]{0,1}));
-        assertEquals(0, TrappingWater.compute(new int[]{0,0}));
-        assertEquals(0, TrappingWater.compute(new int[]{0,1,4}));
-        assertEquals(0, TrappingWater.compute(new int[]{423,4322,4322}));
-        assertEquals(1, TrappingWater.compute(new int[]{2,1,4}));
-        assertEquals(59, TrappingWater.compute(new int[]{60,1,89}));
+        assertEquals(0, TrappingWater2.compute(new int[]{}));
+        assertEquals(0, TrappingWater2.compute(new int[]{0}));
+        assertEquals(0, TrappingWater2.compute(new int[]{0,1}));
+        assertEquals(0, TrappingWater2.compute(new int[]{0,0}));
+        assertEquals(0, TrappingWater2.compute(new int[]{0,1,4}));
+        assertEquals(0, TrappingWater2.compute(new int[]{423,4322,4322}));
+        assertEquals(1, TrappingWater2.compute(new int[]{2,1,4}));
+        assertEquals(59, TrappingWater2.compute(new int[]{60,1,89}));
     }
 
 }
