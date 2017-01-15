6dc0051f3ce18a9adbfd4fb7abbca9e5e86fad6d
Andrey Demyanenko <andrewdem13@gmail.com>
2017-01-15 14:21:41 +0200

    Update TrappingWater3D.java

--- TrappingWater3D-Algorithm/TrappingWater3D.java
+++ TrappingWater3D-Algorithm/TrappingWater3D.java
@@ -15,18 +15,14 @@
  */
 public class TrappingWater3D {
     public static void main(String[] args) {
-        int[] line1 = new int[]{9,9,9,9,9};
-        int[] line2 = new int[]{9,2,1,2,9};
-        int[] line3 = new int[]{9,2,8,2,9};
-        int[] line4 = new int[]{9,2,3,2,9};
-        int[] line5 = new int[]{9,9,9,9,9};
-        int[][] map = new int[5][5];
+        int[] line1 = new int[]{1,4,3,1,3,2};
+        int[] line2 = new int[]{3,2,1,3,2,4};
+        int[] line3 = new int[]{2,3,3,2,3,1};
+        int[][] map = new int[3][6];
         map[0] = line1;
         map[1] = line2;
         map[2] = line3;
-        map[3] = line4;
-        map[4] = line5;
-        System.out.print(compute(map));
+        System.out.println(compute(map));
     }
 
     public static int compute(int[][] map) {
