202786c7975de4eeec092f8b192abc5095c847a7
Andrey Demyanenko <andrewdem13@gmail.com>
2017-01-18 16:53:43 +0200

    Update and rename Solution.java to TagSearcher.java

--- htmlTagSearcher-Algorithm/TagSearcher.java
+++ htmlTagSearcher-Algorithm/TagSearcher.java
@@ -1,14 +1,12 @@
-package com.javarush.test.level19.lesson10.bonus03;
-
 /* Знакомство с тегами
-Считайте с консоли имя файла, который имеет HTML-формат
-Пример:
+Считать с консоли имя файла, который имеет HTML-формат
+Пример содержания файла:
 Info about Leela <span xml:lang="en" lang="en"><b><span>Turanga Leela
 </span></b></span><span>Super</span><span>girl</span>
 Первым параметром в метод main приходит тег. Например, "span"
+
 Вывести на консоль все теги, которые соответствуют заданному тегу
 Каждый тег на новой строке, порядок должен соответствовать порядку следования в файле
-Количество пробелов, \n, \r не влияют на результат
 Файл не содержит тег CDATA, для всех открывающих тегов имеется отдельный закрывающий тег, одиночных тегов нету
 Тег может содержать вложенные теги
 Пример вывода:
@@ -16,14 +14,6 @@ Info about Leela <span xml:lang="en" lang="en"><b><span>Turanga Leela
 <span>Turanga Leela</span>
 <span>Super</span>
 <span>girl</span>
-
-Шаблон тега:
-<tag>text1</tag>
-<tag text2>text1</tag>
-<tag
-text2>text1</tag>
-
-text1, text2 могут быть пустыми
 */
 
 import java.io.BufferedReader;
